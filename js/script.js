321891abd1889afb9c86fc9f1b56213cab48d5b3
dindukuc <chetan.dindukurthi@gmail.com>
2023-07-19 23:54:58 -0700

    was able to get the axis to show up

--- js/script.js
+++ js/script.js
@@ -1,46 +1,51 @@
-// async function parse_year(data){
-//     const parseDate = d3.timeParse("%Y");
-//     data.forEach(d => {
-//         d.Year = parseDate(d.Year);
-//         for (const key in d) {
-//         if (key !== "Year") {
-//             d[key] = +d[key];
-//         }
-//         }
-//     });
-
-//     return data;
-// }
-
-
-
-
-
-
-
 async function init() {
+    
+    const width = 1440
+    const height = 800
+    const margin = ({top: 20, right: 30, bottom: 30, left: 40})
+    
+    
     const data = await d3.csv("js/data/videogames_total_sales_per_year.csv");   
-    console.log(data);
+    console.log(data);  
+
+    d3.select('#chart').style("border", "2px solid black");
+
+    console.log(d3.max(data, d => d3.max([d.Nintendo, d.Other, d.PC, d.Sega, d.Sony, d.Xbox])));
 
     
-
-
     //create and place x and y axes
-    var y = d3.scaleLog().domain([10, 150]).range([200, 0]);
-    var x = d3.scaleLog().domain([10, 150]).range([0, 200]);
-
-    d3.select('#chart')
-      .append("g")
-      .attr("transform", "translate(50,50)")
-      .call(d3.axisLeft(y).tickValues([10,20,50,100]).tickFormat(d3.format("~s")) );
-
-    d3.select('#chart')
-      .append("g")
-      .attr("transform", "translate(50,250)")
-      .call(d3.axisBottom(x).tickValues([10,20,50,100]).tickValues([10,20,50,100]).tickFormat(d3.format("~s")));
+    const parse_year = d3.timeParse("%Y");
+    var x_axis = d3.scaleTime()
+        .domain(
+            d3.extent(data, d => parse_year(d.Year))
+        )
+        .range([0, width]);
     
-       
 
+
+    var y_axis = d3.scaleLinear()
+        .domain([
+            0, d3.max(data, d => d3.max([d.Nintendo, d.Other, d.PC, d.Sega, d.Sony, d.Xbox]))
+        ])
+        .range([height, 0]);
+
+
+    d3.select('#chart')
+        .append("g")
+        .attr("transform", "translate(40, 825)")
+        .call(
+            d3.axisBottom(x_axis) 
+        );
+
+
+    d3.select('#chart')
+      .append("g")
+      .attr("transform", "translate(40,25)")
+      .call(d3.axisLeft(y_axis).tickFormat(d3.format("~s")) );
+
+   
+    
+    
 }
 
 
