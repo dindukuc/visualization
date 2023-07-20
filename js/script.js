f0fa1425b452de2f7a2f897de29a9f40fca12f37
dindukuc <chetan.dindukurthi@gmail.com>
2023-07-19 20:01:44 -0700

    added caching prevention and outline around svg

--- js/script.js
+++ js/script.js
@@ -3,20 +3,9 @@
 async function init() {
     const data = await d3.csv("js/data/videogames_total_sales_per_year.csv");
 
+    //create and place x and y axes
     var y = d3.scaleLog().domain([10, 150]).range([200, 0]);
     var x = d3.scaleLog().domain([10, 150]).range([0, 200]);
-
-    // console.log(data)
-
-    d3.select('svg').append("g").attr("transform", "translate(50,50)")
-    .selectAll("circle")
-    .data(data)  
-    .enter()  
-      .append("circle")
-        .attr("cx", function(d){ return x(parseInt(d.AverageCityMPG)) })
-        .attr("cy", function(d){return y(parseInt(d.AverageHighwayMPG)) }) 
-        .attr("r", function(d){return 2 + parseInt(d.EngineCylinders) });
-
 
     d3.select('svg')
       .append("g")
@@ -28,14 +17,7 @@ async function init() {
       .attr("transform", "translate(50,250)")
       .call(d3.axisBottom(x).tickValues([10,20,50,100]).tickValues([10,20,50,100]).tickFormat(d3.format("~s")));
     
-    
-    
-
-
-
-
-
-
+       
 
 }
 
